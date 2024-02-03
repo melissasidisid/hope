956df2a0cc76ac6d8fbdca6b1866953ab3d88bc8
Walid Belharazem <138311484+walidbelharazem@users.noreply.github.com>
2024-02-03 19:36:12 +0100

    Update layout.js

--- gloria/src/app/layout.js
+++ gloria/src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.png" />
+        <link rel="icon" href="logo.png" />
       </head>
       <body className={inter.className}>
         <UseClient />
