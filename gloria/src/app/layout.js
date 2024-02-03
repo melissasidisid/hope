1ef89faf73bfeec3e338db113ec5d07c8e91cfad
Walid Belharazem <138311484+walidbelharazem@users.noreply.github.com>
2024-02-03 19:09:54 +0100

    Update layout.js
    edit the favicon

--- gloria/src/app/layout.js
+++ gloria/src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body className={inter.className}>
         <UseClient />
         {children}
